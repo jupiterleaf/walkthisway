a50ac99b6a02d6d03d92656ebd95c6881bb24e6e
jupiterleaf <pikaq.snail@gmail.com>
2018-08-25 05:07:53 +1000

    3rd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,11 +102,6 @@
                     <td>2.2</td>
                     <td>2.3</td>
                 </tr>
-                <tr>
-                    <td>3.1</td>
-                    <td>3.2</td>
-                    <td>3.3</td>
-                </tr>
             </table>
         </div>
     </body>
